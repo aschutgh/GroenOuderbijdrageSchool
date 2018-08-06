5a334efe4af41c8aff398338beab00dbcb3e02d4
Arthur Schut <aschutgh@gmail.com>
2018-08-06 12:54:00 +0200

    Functie om ouderbijdrage te berekenen toegevoegd

--- GroenOuderbijdrageSchool/Program.cs
+++ GroenOuderbijdrageSchool/Program.cs
@@ -54,9 +54,51 @@ namespace GroenOuderbijdrageSchool
             tkinderen.Add(nieuwkind);
         }
 
+        static Double berekenBijdrage(List<Kind> kinderen, bool eenouder)
+        {
+            var aantaljdtien = 0; // aantal kinderen jonger dan tien
+            var aantalouderdt = 0; // aantal kinderen ouder dan tien
+            Double ouderbijdrage = 50.0; // Basisbijdrage is 50 euro.
+
+            foreach(Kind individu in kinderen)
+            {
+                if (individu.Jongertien == true)
+                {
+                    aantaljdtien += 1;
+                }
+                else
+                {
+                    aantalouderdt += 1;
+                }
+            }
+            // meer dan drie kinderen jonger dan tien, is niet relevant voor berekenen ouderbijdrage
+            if (aantaljdtien > 3)
+            {
+                aantaljdtien = 3;
+            }
+            // meer dan twee kinderen ouder dan tien jaar, is niet relevant voor berekenen ouderbijdrage
+            if (aantalouderdt > 2)
+            {
+                aantalouderdt = 2;
+            }
+            ouderbijdrage = ouderbijdrage + (aantaljdtien * 25) + (aantalouderdt * 37);
+            // maximale bijdrage van 150 euro
+            if (ouderbijdrage > 150)
+            {
+                ouderbijdrage = 150;
+            }
+            // eenouder gezin? dan 25 procent korting
+            if (eenouder == true)
+            {
+                ouderbijdrage = 0.75 * ouderbijdrage;
+            }
+            return ouderbijdrage;
+        }
+
         static void Main(string[] args)
         {
             List<Kind> kinderen = new List<Kind>();
+            bool eenouder = false;
 
             Console.WriteLine("Uitrekenen van de ouderbijdrage voor school.");
             var meerkinderen = true;
@@ -70,11 +112,19 @@ namespace GroenOuderbijdrageSchool
                 }
             }
 
+            Console.WriteLine("Opgegeven kinderen: ");
             foreach(Kind individu in kinderen)
             {
                 Console.WriteLine(individu);
             }
 
+            Console.WriteLine("Is het gezin een eenoudergezin? (ja of nee) ");
+            if (Console.ReadLine() == "ja")
+            {
+                eenouder = true;
+            }
+
+            Console.WriteLine("De ouderbijdrage bedraagt: {0}", berekenBijdrage(kinderen, eenouder));
         }
     }
 }
