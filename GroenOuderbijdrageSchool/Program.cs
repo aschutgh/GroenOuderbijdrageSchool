c9d9da41f43f99a65bde978c630468921f5c72f7
Arthur Schut <aschutgh@gmail.com>
2018-08-06 12:18:38 +0200

    List gedeclareerd om kinderen in op te slaan

--- GroenOuderbijdrageSchool/Program.cs
+++ GroenOuderbijdrageSchool/Program.cs
@@ -6,10 +6,15 @@ namespace GroenOuderbijdrageSchool
 {
     public class Kind
     {
+        public DateTime Gebdat { get; set; }
+        public DateTime Peildat { get; set; }
+        public bool Jongertien { get; set; }
+        public Kind() { }
         public Kind(DateTime gebdat, DateTime peildat)
         {
             Gebdat = gebdat;
             Peildat = peildat;
+            //Jongertien = false;
             if (DateTime.Compare(Gebdat.AddYears(10), Peildat) < 0)
             {
                 Jongertien = false;
@@ -19,11 +24,6 @@ namespace GroenOuderbijdrageSchool
                 Jongertien = true;
             }
         }
-
-        public DateTime Gebdat { get; set; }
-        public DateTime Peildat { get; set; }
-        public bool Jongertien { get; set; }
-
         public override string ToString()
         {
             return Gebdat + " " + Peildat + " " + "Jonger dan tien? " + Jongertien;
@@ -45,15 +45,36 @@ namespace GroenOuderbijdrageSchool
             return Console.ReadLine();
         }
 
-        static void Main(string[] args)
+        static void vraagKindInfo(ref List<Kind> tkinderen)
         {
-            Console.WriteLine("Uitrekenen van de ouderbijdrage voor school.");
-
             DateTime gebdat = DateTime.Parse(vraagGebdat());
             DateTime peildat = DateTime.Parse(vraagPeildat());
 
             var nieuwkind = new Kind(gebdat, peildat);
-            Console.WriteLine("Je hebt zojuist dit kind ingevoerd: {0} ", nieuwkind);
+            tkinderen.Add(nieuwkind);
+        }
+
+        static void Main(string[] args)
+        {
+            List<Kind> kinderen = new List<Kind>();
+
+            Console.WriteLine("Uitrekenen van de ouderbijdrage voor school.");
+            var meerkinderen = true;
+            while (meerkinderen == true)
+            {
+                vraagKindInfo(ref kinderen);
+                Console.WriteLine("Wil je meer kinderen invoeren? (ja of nee) ");
+                if (Console.ReadLine() == "nee")
+                {
+                    meerkinderen = false;
+                }
+            }
+
+            foreach(Kind individu in kinderen)
+            {
+                Console.WriteLine(individu);
+            }
+
         }
     }
 }
