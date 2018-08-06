4b812e4e5d48cf7533bb3d954b1b14309372b919
Arthur Schut <aschutgh@gmail.com>
2018-08-06 10:14:59 +0200

    Add project files.

---
+++ GroenOuderbijdrageSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroenOuderbijdrageSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
