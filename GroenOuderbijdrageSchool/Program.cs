126429b0609cf3a8830f682a19330df5bb4c22bd
Arthur Schut <aschutgh@gmail.com>
2018-08-06 11:01:29 +0200

    Kind datastructuur geschreven

--- GroenOuderbijdrageSchool/Program.cs
+++ GroenOuderbijdrageSchool/Program.cs
@@ -4,6 +4,33 @@ using System.Collections.Generic;
 
 namespace GroenOuderbijdrageSchool
 {
+    public class Kind
+    {
+        public Kind(DateTime gebdat, DateTime peildat)
+        {
+            Gebdat = gebdat;
+            Peildat = peildat;
+            if (DateTime.Compare(gebdat.AddYears(10), peildat) < 0)
+            {
+                Jongertien = true;
+            }
+            else
+            {
+                Jongertien = false;
+            }
+        }
+
+        public DateTime Gebdat { get; set; }
+        public DateTime Peildat { get; set; }
+        public bool Jongertien { get; set; }
+
+        public override string ToString()
+        {
+            return Gebdat + " " + Peildat + " " + "Jonger dan tien? " + Jongertien;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
