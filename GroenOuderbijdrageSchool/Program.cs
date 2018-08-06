ebbbbf510c4a18beffdb009b21e278e51424d27a
Arthur Schut <aschutgh@gmail.com>
2018-08-06 11:17:01 +0200

    Kind datastructuur werkt correct

--- GroenOuderbijdrageSchool/Program.cs
+++ GroenOuderbijdrageSchool/Program.cs
@@ -10,13 +10,13 @@ namespace GroenOuderbijdrageSchool
         {
             Gebdat = gebdat;
             Peildat = peildat;
-            if (DateTime.Compare(gebdat.AddYears(10), peildat) < 0)
+            if (DateTime.Compare(Gebdat.AddYears(10), Peildat) < 0)
             {
-                Jongertien = true;
+                Jongertien = false;
             }
             else
             {
-                Jongertien = false;
+                Jongertien = true;
             }
         }
 
@@ -33,9 +33,27 @@ namespace GroenOuderbijdrageSchool
 
     class Program
     {
+        static string vraagGebdat()
+        {
+            Console.Write("Geef geboortedatum van kind in formaat jjjj/mm/dd: ");
+            return Console.ReadLine();
+        }
+
+        static string vraagPeildat()
+        {
+            Console.Write("Geef peildatum in formaat jjjj/mm/dd: ");
+            return Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Uitrekenen van de ouderbijdrage voor school.");
+
+            DateTime gebdat = DateTime.Parse(vraagGebdat());
+            DateTime peildat = DateTime.Parse(vraagPeildat());
+
+            var nieuwkind = new Kind(gebdat, peildat);
+            Console.WriteLine("Je hebt zojuist dit kind ingevoerd: {0} ", nieuwkind);
         }
     }
 }
